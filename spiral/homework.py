054990410d987f10cafc62f59c96b5f085dadd8e
Collin Sandler <sandlercp@vcu.edu>
2021-04-09 14:11:22 -0400

    Cleaned up the final homework, got rid of return value to pass pylint test

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,14 +1,13 @@
 def spiralize(number): #creating the function called spiralize
     n = (number - 1) / 2 #counting diagonals minus the center (1)
-
-    return_value = 1 #initial return value is 1
     
     x=( 3 + 2 * n * (8 * n * n + 15 * n + 13)) / 3 #used github to find this framework
-    return_value = x #had trouble with this one at first, but assigned x before return_value and it worked
-    return return_value
+    return x
 
 def SumDiagonals():
     diagonal = spiralize(501) #this is a 501x501 matrix
 
     print(diagonal)
-SumDiagonals()
+
+SumDiagonals() #using function created above to sum the diagonals in the spiralize() function
+#This makes n=250 because (501-1)/2 = 250
